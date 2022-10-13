055514815131b56039f312b6977d1222256b281c
Team-Paradise-1165 <107153539+Team-Paradise-1165@users.noreply.github.com>
2022-10-12 17:04:05 -0700

    starting new branch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,11 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final int rightMotor1_num = 0; 
+    public static final int rightMotor2_num = 1;
+    public static final int leftMotor1_num = 2;
+    public static final int leftMotor2_num = 3;
+
+    public static final int joystick_num = 0;
+}
